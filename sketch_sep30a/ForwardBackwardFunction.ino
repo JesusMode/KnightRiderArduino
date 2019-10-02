316b377c7cacd0293f6844ef24521771e6e7c49b
XxChaoticStrike <55609463+XxChaoticStrike@users.noreply.github.com>
2019-10-02 09:14:41 -0400

    Updated code to add additional function

--- sketch_sep30a/ForwardBackwardFunction.ino
+++ sketch_sep30a/ForwardBackwardFunction.ino
@@ -1,5 +1,7 @@
-void lightsForward(){
-    for (int x = 0; x < z; x++) {
+int c = 0;
+
+void lightsForward() {
+  for (int x = 0; x < z; x++) {
     digitalWrite(x, HIGH); \
     digitalWrite(x + 1, HIGH);
     delay(y);
@@ -8,12 +10,27 @@ void lightsForward(){
   } // End of for loop; going forward
 }
 
-void lightsBackward(){
-    for (int x = z; x > -1; x -= 1) {
+void lightsBackward() {
+
+  for (int x = z; x > -1; x -= 1) {
     digitalWrite(x, HIGH);
     digitalWrite(x - 1, HIGH);
     delay(y);
     digitalWrite(x, LOW);
     digitalWrite(x - 1, LOW);
   } // End of for loop; going backward
+  c = c + 1;
+  changeSpeed();
+}
+
+void changeSpeed() {
+  if (c >= 1 && c <= 6) {
+    y += 10;
+  }
+  if (c >= 6 && c <= 11) {
+    y -= 10;
+  }
+  if (c >= 12){
+    c = 0;
+  }
 }
