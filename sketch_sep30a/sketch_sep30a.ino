f3b181575bb730af9a839e69a0664034468991f3
XxChaoticStrike <55609463+XxChaoticStrike@users.noreply.github.com>
2019-10-03 09:15:06 -0400

    Updated code
    Added criss cross function + potentiometer support

--- sketch_sep30a/sketch_sep30a.ino
+++ sketch_sep30a/sketch_sep30a.ino
@@ -1,13 +1,19 @@
-float y = 62.5; // Delay of light switch
+int y; // Delay of light switch
 int z = 10; // Pin Counter
 
 void setup() {
   for (int x = 0; x < 14; x++) {
     pinMode(x, OUTPUT);
-  }
-} // 8 lights total, end of setup
+    
+  } // Initializes all the pins as OUTPUT. End of FOR loop
+
+} // 10 lights total, end of setup
 
 void loop() {
+  y = analogRead(A1);
+  map(y, 0, 1023, 40, 500);
+  crissCrossGO();
   lightsForward();
   lightsBackward();
-} // End of void loop()
+
+}
